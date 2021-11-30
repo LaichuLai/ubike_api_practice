515ddbf5833677f600cf9991b1b4d0e965fe91e1
ShibaDogge <cozzichilli@gmail.com>
2021-11-30 09:30:37 +0800

    修改JS檔案內容

--- ubikeAPI.js
+++ ubikeAPI.js
@@ -9,13 +9,23 @@ $().ready(() => {
 
 
   searchBtn.addEventListener("click", () => {
-    if (searchBike.value !== "") {
+
+    const bikeStop = searchBike.value
+    if ( bikeStop !== "") {
       $.ajax({
         url: "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json",
         type: "GET",
         dataType: "json",
         success: function(ubike) {
       
+
+          console.log(ubike[i].sna);
+          console.log(ubike[i].sbi);
+          console.log(ubike[i].bemp);
+          console.log(ubike[i].ar);
+          console.log(ubike[i].aren);
+          console.log(ubike[i].srcUpdateTime);
+
           var NumOfData = ubike.length;
           console.log(NumOfData);
       
